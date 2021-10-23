647b1febea0878598c5f3cca7ce10b5829845ec1
Izel Yazici <61404791+izelyzc@users.noreply.github.com>
2021-10-23 21:01:45 +0300

    Add files via upload

---
+++ InClassExercises/polymorphism.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 20:32:55 2021
+
+@author: izely
+"""
+class Animal(object):
+  living="Yes!"
+  def __init__(self, name):    # Constructor of the class
+      self.name = name
+      
+  def talk(self):              # Abstract method, defined by convention only
+  	raise NotImplementedError("Subclass must implement abstract method")
+  	 
+class Cat(Animal):
+  def talk(self):
+    return self.meow()
+    
+  def meow(self):
+    return 'Meow!'
+ 
+class Dog(Animal):
+  def talk(self):
+    return self.bark()
+  
+  def bark(self):
+    return 'Woof! Woof!'
+      
+class Fish(Animal):
+  
+  def swim(self):
+    pass
+  
+  def __str__(self):
+    return "I am a fish!"
+      
+animals = [Cat('Foo'),
+           Dog('Bar'),
+           Fish('nemo')]
+ 
+# for animal in animals:
+#   print(animal.name + ': ' + animal.talk())
+  
+# f = Fish("foo")
+# print("Hi, " + str(f))
